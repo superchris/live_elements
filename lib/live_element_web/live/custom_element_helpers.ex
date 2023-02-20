379492ac0c0ecc84d54c90cd9189994522ddabae
Chris Nelson <superchrisnelson@gmail.com>
2023-02-19 19:26:30 -0500

    working macro

--- lib/live_element_web/live/custom_element_helpers.ex
+++ lib/live_element_web/live/custom_element_helpers.ex
@@ -1,36 +1,5 @@
 defmodule LiveElementWeb.CustomElementHelpers do
-  import Phoenix.Component
-  alias Phoenix.HTML.Tag
-  alias Phoenix.HTML
 
-  ce_manifest_path = "#{__DIR__}/../../../assets/custom-elements.json"
-  %{"modules" => modules} = File.read!(ce_manifest_path) |> Jason.decode!()
-  for custom_element <- LiveElements.Helpers.find_custom_elements(modules) do
-    %{"tagName" => tag_name, "events" => events} = custom_element
-    function_name = tag_name |> String.replace("-", "_") |> String.to_atom()
-    def unquote(function_name)(var!(assigns)) do
-      custom_element_tag(assigns, unquote(custom_element |> Macro.escape()))
-      # events = unquote(events |> Macro.escape())
-      # tag_name = unquote(tag_name)
-      # attrs = assigns_to_attributes(assigns) |> serialize()
-      # event_names = events |> Enum.map(& &1["name"]) |> Enum.join(",")
-      # assigns = assigns |> assign(tag_name: tag_name, attrs: attrs, events: event_names)
-      # ~H"""
-      # <.dynamic_tag name={@tag_name} {@attrs} phx-hook="PhoenixCustomEventHook" phx-send-events={@events}></.dynamic_tag>
-      # """
-    end
-  end
+  use LiveElements.Helpers
 
-  def custom_element_tag(assigns, %{"tagName" => tag_name, "events" => events}) do
-    attrs = assigns_to_attributes(assigns) |> serialize()
-    event_names = events |> Enum.map(& &1["name"]) |> Enum.join(",")
-    assigns = assigns |> assign(tag_name: tag_name, attrs: attrs, events: event_names)
-    ~H"""
-    <.dynamic_tag name={@tag_name} {@attrs} phx-hook="PhoenixCustomEventHook" phx-send-events={@events}></.dynamic_tag>
-    """
-  end
-
-  def serialize(assigns) do
-    assigns |> Enum.map(fn {key, value} -> {key, Jason.encode!(value)} end)
-  end
 end
--- lib/live_elements_helpers.ex
+++ lib/live_elements_helpers.ex
@@ -1,32 +1,26 @@
 defmodule LiveElements.Helpers do
   import Phoenix.Component
-  import Phoenix.LiveView
+  alias LiveElements.Helpers
 
   defmacro __using__(_opts) do
-    quote do
-      import Phoenix.Component
-      import Phoenix.LiveView
-      alias LiveElements.Helpers
 
-      for custom_element <- Helpers.custom_elements() do
-        %{"tagName" => tag_name, "events" => events} = custom_element
-        function_name = Helpers.function_name(custom_element)
-        # def unquote(function_name)(assigns) do
-        #   events = unquote(events |> Macro.escape())
-        #   tag_name = unquote(tag_name)
-        #   attrs = assigns_to_attributes(var!(assigns)) |> LiveElements.Helpers.serialize()
-        #   event_names = events |> Enum.map(& &1["name"]) |> Enum.join(",")
-        #   assigns |> assign(tag_name: tag_name, attrs: attrs, events: event_names)
+    for custom_element <- Helpers.custom_elements() do
+      %{"tagName" => tag_name, "events" => events} = custom_element
+      function_name = function_name(tag_name)
 
-        #   ~H"""
-        #   <.dynamic_tag name={@tag_name} {@attrs} phx-hook="PhoenixCustomEventHook" phx-send-events={@events}></.dynamic_tag>
-        #   """
-        # end
+      quote do
+        def unquote(function_name)(assigns) do
+          Helpers.custom_element_tag(
+            assigns,
+            unquote(tag_name),
+            unquote(events |> Macro.escape())
+          )
+        end
       end
     end
   end
 
-  def function_name(%{"tagName" => tag_name}) do
+  def function_name(tag_name) do
     tag_name |> String.replace("-", "_") |> String.to_atom()
   end
 
@@ -45,6 +39,15 @@ defmodule LiveElements.Helpers do
     |> Enum.map(& &1["declarations"])
     |> Enum.concat()
     |> Enum.filter(& &1["customElement"])
-    |> IO.inspect()
+  end
+
+  def custom_element_tag(assigns, tag_name, events) do
+    attrs = assigns_to_attributes(assigns) |> serialize()
+    event_names = events |> Enum.map(& &1["name"]) |> Enum.join(",")
+    assigns = assigns |> assign(tag_name: tag_name, attrs: attrs, events: event_names)
+
+    ~H"""
+    <.dynamic_tag name={@tag_name} {@attrs} phx-hook="PhoenixCustomEventHook" phx-send-events={@events}></.dynamic_tag>
+    """
   end
 end
